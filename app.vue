63dfbcecf9150beb23e11781bbf86f149c0860f1
P Mohith <96006193+I-am-Mohith@users.noreply.github.com>
2023-07-17 15:48:24 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nav class="border-red-500">
+      <div class="flex flex-row gap-3 w-10">
+       <img src="/assets/icons/logo.svg" alt="">
+      </div>
+    </nav>
+  </div>
+</template>
